3cf63a722867e808189c93fd841446d3b8e0f176
seongsuPark <sungsu9022@naver.com>
2015-06-07 14:15:47 +0900

    기본 뼈대 구성 완료

---
+++ app/src/main/java/com/connectu/mody/login/activity/LoginActivity.java
@@ -0,0 +1,103 @@
+package com.connectu.mody.login.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.connectu.mody.MainActivity;
+import com.connectu.mody.R;
+import com.connectu.mody.login.controller.SharedPreferenceController;
+
+
+public class LoginActivity extends Activity {
+    EditText editText_id;
+    EditText editText_pw;
+    String string_id;
+    String string_pw;
+    Boolean boolean_yn;
+    Button login_but;
+    Button join_but;
+    CheckBox checkBox_autologin;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        editText_id = (EditText) findViewById(R.id.edittext_id);
+        editText_pw = (EditText) findViewById(R.id.edittext_password);
+
+        login_but = (Button) findViewById(R.id.button_login);
+        join_but = (Button) findViewById(R.id.button_join);
+
+        checkBox_autologin = (CheckBox) findViewById(R.id.checkbox_autologin);
+
+        SharedPreferenceController pref = new SharedPreferenceController(LoginActivity.this);
+        checkBox_autologin.setChecked(pref.getValue(SharedPreferenceController.PREF_USER_AUTOLOGIN, false));
+
+        if(checkBox_autologin.isChecked())
+        {
+            string_id = pref.getValue(SharedPreferenceController.PREF_USER_ID, null);
+            string_pw = pref.getValue(SharedPreferenceController.PREF_USER_PASSWORD, null);
+
+            editText_id.setText(string_id);
+            editText_pw.setText(string_pw);
+
+            if(editText_id.getText().toString().equals("id") && editText_pw.getText().toString().equals("pw"))
+            {
+                Toast.makeText(LoginActivity.this, "Login Success!", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                finish();
+            }
+            else
+            {
+                Toast.makeText(LoginActivity.this,"Login Failed", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        login_but.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View v) {
+                try {
+                    if (editText_id.getText().toString().equals("id") && editText_pw.getText().toString().equals("pw")) {
+                        Toast.makeText(LoginActivity.this, "Login Success!", Toast.LENGTH_SHORT).show();
+
+                        if (checkBox_autologin.isChecked())
+                        {
+                            SharedPreferenceController pref = new SharedPreferenceController(LoginActivity.this);
+                            pref.put(SharedPreferenceController.PREF_USER_ID, editText_id.getText().toString());
+                            pref.put(SharedPreferenceController.PREF_USER_PASSWORD, editText_pw.getText().toString());
+                            pref.put(SharedPreferenceController.PREF_USER_AUTOLOGIN, true);
+                        }
+                        else
+                        {
+                            SharedPreferenceController pref = new SharedPreferenceController(LoginActivity.this);
+                            pref.put(SharedPreferenceController.PREF_USER_AUTOLOGIN, false);
+                        }
+
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                        finish();
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        join_but.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_join);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/connectu/mody/login/controller/SharedPreferenceController.java
@@ -0,0 +1,86 @@
+package com.connectu.mody.login.controller;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by Bono on 2015-04-11.
+ */
+public class SharedPreferenceController {
+
+    private final String PREF_NAME = "com.connectu.pref";
+
+    public final static String PREF_USER_ID = "PREF_USER_ID";
+    public final static String PREF_USER_PASSWORD = "PREF_USER_PASSWORD";
+    public final static String PREF_USER_AUTOLOGIN = "PREF_USER_AUTOLOGIN";
+
+
+    static Context mContext;
+
+    public SharedPreferenceController(Context c) {
+        mContext = c;
+    }
+
+    public void put(String key, String value) {
+        SharedPreferences pref = mContext.getSharedPreferences(PREF_NAME,
+                Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+
+        editor.putString(key, value);
+        editor.commit();
+    }
+
+    public void put(String key, boolean value) {
+        SharedPreferences pref = mContext.getSharedPreferences(PREF_NAME,
+                Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+
+        editor.putBoolean(key, value);
+        editor.commit();
+    }
+
+    public void put(String key, int value) {
+        SharedPreferences pref = mContext.getSharedPreferences(PREF_NAME,
+                Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+
+        editor.putInt(key, value);
+        editor.commit();
+    }
+
+    public String getValue(String key, String dftValue) {
+        SharedPreferences pref = mContext.getSharedPreferences(PREF_NAME,
+                Activity.MODE_PRIVATE);
+
+        try {
+            return pref.getString(key, dftValue);
+        } catch (Exception e) {
+            return dftValue;
+        }
+
+    }
+
+    public int getValue(String key, int dftValue) {
+        SharedPreferences pref = mContext.getSharedPreferences(PREF_NAME,
+                Activity.MODE_PRIVATE);
+
+        try {
+            return pref.getInt(key, dftValue);
+        } catch (Exception e) {
+            return dftValue;
+        }
+
+    }
+
+    public boolean getValue(String key, boolean dftValue) {
+        SharedPreferences pref = mContext.getSharedPreferences(PREF_NAME,
+                Activity.MODE_PRIVATE);
+
+        try {
+            return pref.getBoolean(key, dftValue);
+        } catch (Exception e) {
+            return dftValue;
+        }
+    }
+}
